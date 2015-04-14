b591410a438821b5743d6c7d85666ff80600f1f5
Koen Claessen <koen@chalmers.se>
2015-04-14 09:38:31 +0200

    work terms

--- SAT/Term.hs
+++ SAT/Term.hs
@@ -1,5 +1,11 @@
 module SAT.Term where
 
-import SAT
+import SAT as S
+import SAT.Unary as U
+import SAT.Binary as B
 
 ------------------------------------------------------------------------------
+
+data Term = Term [(Integer,Lit)] Integer
+
+
